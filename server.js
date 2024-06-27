49562abc1ac1f443a379afd2bf327b3971f68222
yunus-EmreX <138483345+yunus-EmreX@users.noreply.github.com>
2024-06-27 18:59:19 +0300

    Update and rename tempermonkey.js to server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (req, res) => {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        console.log('Received data:', body);
+        res.end('ok');
+    });
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('Something went wrong:', err);
+    }
+    console.log(`Server is listening on port ${port}`);
+});
